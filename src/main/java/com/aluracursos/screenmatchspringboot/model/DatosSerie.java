3e90c137c4227f8fbe9174a99047a0b90d25edfe
Gerardo <everythingissofast@gmail.com>
2024-06-27 16:20:48 -0600

    implementacion de clase datosserie, datos temporadas y datos episodios

--- src/main/java/com/aluracursos/screenmatchspringboot/model/DatosSerie.java
+++ src/main/java/com/aluracursos/screenmatchspringboot/model/DatosSerie.java
@@ -7,5 +7,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 //json properties es para leer y escribir
 
 @JsonIgnoreProperties(ignoreUnknown = true) //esta anotacion es para que ignore los demas atributos que no estamos llamando en el metodo
-public record DatosSerie(@JsonAlias("Title")  String titulo,@JsonAlias("totalSeasons") Integer totalDeTemporadas, @JsonAlias("imdbRating") String evaluacion) {
+public record DatosSerie(@JsonAlias("Title")  String titulo,
+                         @JsonAlias("totalSeasons") Integer totalDeTemporadas,
+                         @JsonAlias("imdbRating") String evaluacion) {
 }
--- src/main/java/com/aluracursos/screenmatchspringboot/service/IConvierteDatos.java
+++ src/main/java/com/aluracursos/screenmatchspringboot/service/IConvierteDatos.java
@@ -3,5 +3,4 @@ package com.aluracursos.screenmatchspringboot.service;
 public interface IConvierteDatos {
     <T> T obtenerDatos(String json,Class<T> clase);
 
-
 }
